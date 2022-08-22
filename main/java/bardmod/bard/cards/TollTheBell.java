057d4193a4687c8d27ec6bcd0ddda8fbefe11a02
Lex <alex.j.stuart@gmail.com>
2022-08-22 18:40:24 +0100

    Add Toll the Bell

---
+++ main/java/bardmod/bard/cards/TollTheBell.java
@@ -0,0 +1,49 @@
+package bardmod.bard.cards;
+
+import bardmod.BardMod;
+import bardmod.bard.BardCardTags;
+import bardmod.bard.BardColor;
+import bardmod.bard.ScaleHelper;
+import basemod.abstracts.CustomCard;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+public class TollTheBell extends CustomCard {
+    public static final String ID = "DrumBeat";
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String NAME = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+    private static final int COST = 2;
+    private static final int ATTACK_DMG = 13;
+    private static final int UPGRADE_PLUS_DMG = 3;
+
+
+    public TollTheBell() {
+        super(ID, NAME, BardMod.makeCardImagePath(ID), COST, DESCRIPTION, CardType.ATTACK, BardColor.BARD_ORANGE, CardRarity.COMMON, CardTarget.ENEMY);
+
+        this.baseDamage = ATTACK_DMG;
+        this.baseMagicNumber = ENERGY_AMOUNT;
+        tags.add(BardCardTags.NOTE_A);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractDungeon.actionManager.addToBottom(new com.megacrit.cardcrawl.actions.common.DamageAction(m,
+                new DamageInfo(p, this.damage, this.damageTypeForTurn),
+                AbstractGameAction.AttackEffect.SLASH_DIAGONAL));
+    }
+
+    @Override
+    public void upgrade() {
+        if (!this.upgraded) {
+            upgradeName();
+            upgradeDamage(UPGRADE_PLUS_DMG);
+        }
+    }
+}
